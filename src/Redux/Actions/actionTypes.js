b7d9c5189f2a19df28c9337d5c33408685e9dc27
João Coqueiro <jaaum.p95@gmail.com>
2022-10-07 16:06:05 -0300

    FEAT: REQ 1

---
+++ src/Redux/Actions/actionTypes.js
@@ -0,0 +1,4 @@
+const GENERIC_DATA = 'GENERIC_DATA';
+const LOGIN_DATA = 'LOGIN_DATA';
+
+export { GENERIC_DATA, LOGIN_DATA };
---
+++ src/components/Input.js
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+export default class Input extends React.Component {
+  render() {
+    const { onChange, type, value, name, children, datatestid } = this.props;
+    return (
+      <div>
+        <label htmlFor={ name }>
+          {children}
+          <input
+            onChange={ onChange }
+            type={ type }
+            value={ value }
+            name={ name }
+            data-testid={ datatestid }
+            id={ name }
+          />
+        </label>
+      </div>
+    );
+  }
+}
+
+Input.propTypes = {
+  children: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  type: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  datatestid: PropTypes.string.isRequired,
+};
