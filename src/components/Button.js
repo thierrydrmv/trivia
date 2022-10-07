b45e58dabfe525567312d75c402ff888ccda6bba
João Coqueiro <jaaum.p95@gmail.com>
2022-10-07 19:19:37 -0300

    FEAT: REQ 6

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,30 +3,30 @@ import React from 'react';
 
 export default class Button extends React.Component {
   render() {
-    const { onClick, value, name, children, datatestid, disabled } = this.props;
+    const { onClick, value, name, children, datatestid, disabled, key } = this.props;
     return (
-      <div>
-        <button
-          type="button"
-          onClick={ onClick }
-          value={ value }
-          name={ name }
-          data-testid={ datatestid }
-          id={ name }
-          disabled={ disabled }
-        >
-          { children }
-        </button>
-      </div>
+      <button
+        type="button"
+        onClick={ onClick }
+        value={ value }
+        name={ name }
+        data-testid={ datatestid }
+        id={ name }
+        disabled={ disabled }
+        key={ key }
+      >
+        { children }
+      </button>
     );
   }
 }
 
 Button.propTypes = {
   children: PropTypes.string.isRequired,
+  datatestid: PropTypes.string.isRequired,
+  disabled: PropTypes.bool.isRequired,
+  key: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
   value: PropTypes.string.isRequired,
-  datatestid: PropTypes.string.isRequired,
-  disabled: PropTypes.bool.isRequired,
 };
