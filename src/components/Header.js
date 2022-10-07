1828588b8edf874afca98fc840ee563e80c0d056
João Coqueiro <jaaum.p95@gmail.com>
2022-10-07 17:07:36 -0300

    FEAT: REQ 5

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import md5 from 'crypto-js/md5';
 
 class Header extends Component {
+  state = {
+    hash: '',
+  };
+
+  componentDidMount() {
+    this.fetchHash();
+  }
+
+  fetchHash = () => {
+    const { perfilData: { gravatarEmail } } = this.props;
+    const hash = md5(gravatarEmail).toString();
+    this.setState({
+      hash,
+    });
+  };
+
   render() {
-    // const { perfilData } = this.props;
+    const { perfilData } = this.props;
+    const { hash } = this.state;
     return (
       <div>
-        <p data-testid="header-player-name">a</p>
+        <p data-testid="header-player-name">{ perfilData.name }</p>
         <img
           data-testid="header-profile-picture"
-          src=""
+          src={ `https://www.gravatar.com/avatar/${hash}` }
           alt="perfil"
         />
-        <p data-testid="header-score">a</p>
+        <p data-testid="header-score">{ perfilData.score }</p>
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
-  perfilData: state,
+  perfilData: state.loginReducer,
 });
 
 export default connect(mapStateToProps)(Header);
