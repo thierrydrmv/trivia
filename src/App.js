a33436559a68f48a85c440bd0578dfeb06b7b817
João Coqueiro <jaaum.p95@gmail.com>
2022-10-07 22:58:33 -0300

    FEAT: REQ 14, 15, 16 E 18

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Game from './pages/Game';
 import Login from './pages/Login';
 import Settings from './pages/Settings';
 import Feedback from './pages/Feedback';
+import Ranking from './pages/Ranking';
 import './App.css';
 
 export default class App extends React.Component {
@@ -15,6 +16,7 @@ export default class App extends React.Component {
           <Route exact path="/configuracoes" component={ Settings } />
           <Route exact path="/game" component={ Game } />
           <Route exact path="/feedback" component={ Feedback } />
+          <Route exact path="/ranking" component={ Ranking } />
         </Switch>
       </div>
     );
--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,23 +1,62 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
+import Button from '../components/Button';
 
 class Feedback extends React.Component {
+  state = {
+    name: '',
+  };
+
+  ranking = () => {
+    const { history } = this.props;
+    history.push('/ranking');
+  };
+
   render() {
-    const { assertions } = this.props;
+    const { assertions, score } = this.props;
+    const { name } = this.state;
     const minAssertions = 3;
     const feedbackText = assertions < minAssertions ? 'Could be better...' : 'Well Done!';
     return (
       <div>
         <Header />
+        <p data-testid="feedback-total-score">{score}</p>
+        <p data-testid="feedback-total-question">{assertions}</p>
         <p data-testid="feedback-text">{ feedbackText }</p>
+        <Button
+          datatestid="btn-play-again"
+          name="playAgain"
+          value={ name }
+          onClick={ this.playAgain }
+        >
+          Play Again
+        </Button>
+        <Button
+          datatestid="btn-ranking"
+          name="rankingBtn"
+          value={ name }
+          onClick={ this.ranking }
+        >
+          Ranking
+        </Button>
       </div>
     );
   }
 }
 
+Feedback.propTypes = {
+  assertions: PropTypes.number.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+  score: PropTypes.number.isRequired,
+};
+
 const mapStateToProps = (state) => ({
   assertions: state.player.assertions,
+  score: state.player.score,
 });
 
 export default connect(mapStateToProps)(Feedback);
