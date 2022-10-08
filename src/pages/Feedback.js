84a57d41170408dc171d99d95de8047d3513403d
João Coqueiro <jaaum.p95@gmail.com>
2022-10-07 22:18:23 -0300

    FEAT: REQ 11

---
+++ src/pages/Feedback.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Feedback extends React.Component {
+  render() {
+    return (
+      <div>
+        <p data-testid="feedback-text">Well Done!</p>
+      </div>
+    );
+  }
+}
+
+export default Feedback;
